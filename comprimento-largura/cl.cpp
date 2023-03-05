6fe12a2c4aa324f6b4cba554a921cd4b93a0210b
mathstack <gabrielsaron16@gmail.com>
2023-03-05 12:29:13 -0300

    initial-commit

---
+++ comprimento-largura/cl.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+	float comprimento, largura;
+
+	cout << "Digite a largura e a altura: ";
+	cin >> comprimento >> largura;
+	cout << "Area: " << comprimento * largura << " m2";
+
+}
---
+++ input-output/inout.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	char nome[80];
+	cout << "\nWhat's your name? ";
+	cin >> nome;
+
+	cout << "Olá, " << nome << ", Tudo bem?";
+
+	return 0;
+}
